104b1dd6d3e1ae65e2db8a30ca79475c8f89c0be
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-31 21:36:39 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! I am Helen. Nice to see you here! Have a nice day!';
   }
 }
