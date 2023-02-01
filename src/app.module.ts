07e2732c129d6ecb4549743e7a0afa288ec84a70
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-02-01 15:59:53 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule], //Have to link all modules together
   controllers: [AppController],
   providers: [AppService],
 })
