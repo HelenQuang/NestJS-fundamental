6fead7992c3cfe1bdc50180bf4b142f75836feba
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-02-02 16:26:51 +0200

    Change class to interface

--- src/products/products.model.ts
+++ src/products/products.model.ts
@@ -1,9 +1,24 @@
-export class Product {
-  constructor(
-    //Add accessor public to access these arguments outside of this class
-    public id: string,
-    public title: string,
-    public description: string,
-    public price: number,
-  ) {}
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+// export class Product {
+//   constructor(
+//     Add accessor public to access these arguments outside of this class
+//     public id: string,
+//     public title: string,
+//     public description: string,
+//     public price: number,
+//   ) {}
+// }
+
+export interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
 }
