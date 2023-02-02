0c6a74b34104bd8d2b3a9b083de28663b055b0bb
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-02-02 16:26:24 +0200

    Add MongooseModule and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+ConfigModule.forRoot();
 
 @Module({
-  imports: [ProductsModule], //Have to link all modules together
+  imports: [
+    //Have to link all modules together
+    ProductsModule,
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
+    MongooseModule.forRoot(
+      process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
