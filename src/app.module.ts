d892c0233ef6cc942c18f5e36025c98d04d0a214
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-02-05 01:08:51 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 ConfigModule.forRoot();
 
 @Module({
   imports: [
     //Have to link all modules together
-    ProductsModule,
     ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
     MongooseModule.forRoot(
       process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD),
     ),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //For code-first approach
+      sortSchema: true, //For code-first approach
+    }),
+    ProductsModule,
+    UsersModule,
+    AuthModule,
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
